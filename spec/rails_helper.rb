dfa2be815fa55bb18022ef426f4d85b89b55085e
Mariam Gift <natukunda162@gmail.com>
2019-11-05 22:57:05 +0300

    add rspec/support to autoload

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,6 +42,11 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  RSpec.configuration do |config|
+    config.include RequestSpecHelper, type: :request
+  end
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
